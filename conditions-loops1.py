fa7f943ae0907ec1c0f7104de2dc9cba337a3372
sasi_p <sweetpea_th@hotmail.com>
2021-01-21 18:53:05 -0500

    Modified file

--- conditions-loops1.py
+++ conditions-loops1.py
@@ -1,8 +1,7 @@
-#Write a Python program to convert temperatures to and from celsius, fahrenheit.
+#Write a program to ind those numbers which are divisible by 7 and mutiple of 5, between 1500 and 2700(both included).
 
-celsius = float(input("Please enter temperature in celsius: "))
-fahrenheit = (celsius * 1.8) + 32
-fahrenheit = float(round(fahrenheit,2))
-
-print(celsius, "C is", fahrenheit, "in Fahrenheit\n")
-
+new_list = []
+for x in range(1500, 2701):
+    if (x % 7 == 0) and (x % 5 == 0):
+        new_list.append(str(x))
+print(",".join(new_list))
