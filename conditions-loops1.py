ed0f7030c5aad143f721093c48c1e9f799148ed8
sasi_p <sweetpea_th@hotmail.com>
2021-01-31 18:08:45 -0500

    Initial commit for calculator. Modified conditions-loops1 and 3.

--- conditions-loops1.py
+++ conditions-loops1.py
@@ -1,7 +1,13 @@
-#Write a program to ind those numbers which are divisible by 7 and mutiple of 5, between 1500 and 2700(both included).
+#Write a Python program to convert temperatures to and from celsius, fahrenheit.
 
-new_list = []
-for x in range(1500, 2701):
-    if (x % 7 == 0) and (x % 5 == 0):
-        new_list.append(str(x))
-print(",".join(new_list))
+celsius = float(input("Please enter temperature in celsius: "))
+fahrenheit = float(input("Please enter temperature in fahrenheit: "))
+
+to_fahrenheit = (celsius * 1.8) + 32
+to_celsius = (fahrenheit - 32) * .5556
+
+if 
+
+print(celsius, "C is", fahrenheit, "in Fahrenheit\n")
+
+
--- conditions-loops3.py
+++ conditions-loops3.py
@@ -1,18 +1,2 @@
-#Write a Python program to guess a number between 1 to 9.
-import random
-
-user_choice = int(input("Guess the number between 1 to 9: "))
-
-if user_choice > 9 or user_choice < 1:
-    print("Please enter number between 1 to 9 only")
-else:
-    computer_choice = random.randint(1, 9)
-    print(f"The number is: {computer_choice}")
-    
-    
-    if user_choice == computer_choice:
-        print("Well guessed!")
-    else:
-        print("Try again!")
-        
-#Keep working on this to make the program keeps prompting the user for new number.
+def format_name(f_name, l_name).title():
+  
